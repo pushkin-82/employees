5c8c08e04fd4769584e2a838558993e841b71ec0
Serhii Putylin <serhii.putylin@blockpit.io>
2022-05-12 03:24:01 +0300

    create project structure

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Company "OLOLO" employees</h1>
+            <h2>Total employees: </h2>
+            <h2>Bonus for: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import EmployeeListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeeList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeeListItem />
+            <EmployeeListItem />
+            <EmployeeListItem />
+        </ul>
+    )
+}
+
+export default EmployeeList;
